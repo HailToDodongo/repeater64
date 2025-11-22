9f86403f1613887a2434dfa1fc116d67b6ac871d
Max Bebök <beboek.max@gmail.com>
2025-11-22 18:24:47 +0100

    new: 1-cycle sync test

--- src/demos/RDPNoSync1C.cpp
+++ src/demos/RDPNoSync1C.cpp
@@ -13,10 +13,6 @@
 namespace
 {
   constinit RDPDumpTest dumpTest{
-    .testCases = std::array<uint32_t, 20>({
-      0,1,2,3,4,5,6,7,8,9,10,
-      11,12,13,14,15,16,17,18,19,
-    }),
     .testRegion = std::array<int, 4>({
       16, 48, SCREEN_WIDTH-16, SCREEN_HEIGHT-48
     })
